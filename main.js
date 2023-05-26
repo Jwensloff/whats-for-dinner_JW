9ea48a626f171bca09906ab9a8243edc9a633575
Jocelyn Wensloff <jewensloff@gmail.com>
2023-05-26 11:31:58 -0600

    JS: fix: fix bug that prevented me from showing hidden text

--- main.js
+++ main.js
@@ -54,10 +54,15 @@ var cookingPot = document.querySelector('.cooking-pot');
 // var displayEntire = document.querySelector('.entire');
 var letsCookButton = document.querySelector('.lets-cook-button');
 var radioButtons = document.getElementsByName('dinner');
-var removeCookingIcon = document.querySelector('right-box');
+var rightBox = document.querySelector('.right-box');
+var foodSuggestion = document.querySelector('.food-suggestion');
+var youShouldMake = document.querySelector('.you-should-make');
+
 //........................event listeners.......................
 
-letsCookButton.addEventListener('click', displayFood);
+letsCookButton.addEventListener('click', function(event){
+        toggle(event)
+});
 
 //........................functions ............................
 var sideMainDessertEntree; 
@@ -66,19 +71,38 @@ function getRandomIndex(array) {
     return Math.floor(Math.random() * array.length);
   }
 
-  function hideCookingPot(){
-  cookingPot.innerHTML=''
+function toggle(){
+  cookingPot.classList.add('hidden');
+  foodSuggestion.classList.remove('hidden');
+  youShouldMake.classList.remove('hidden');
 }
-
 
 // first select the value of the specified 
 function displayFood(){
-    hideCookingPot(); 
+
+
     for(var i=0; i < radioButtons.length; i++){ 
         if(radioButtons[i].checked){
           sideMainDessertEntree = radioButtons[i].value
         }
+            console.log(sideMainDessertEntree)
     } 
-    return sideMainDessertEntree;
-    
-} 
+    return sideMainDessertEntree
+
+//     var randomSide = sides[getRandomIndex(sides)];
+//     var randomMain = mains[getRandomIndex(mains)];
+//     var randomDessert = desserts[getRandomIndex(desserts)];
+
+//     if(sideMainDessertEntree.includes('Side')){
+//     rightBox.innerHTML=  `<h1>You should make:</h1>
+//                          <h2>${randomSide} </h2>`
+//     } else if(sideMainDessertEntree == 'Main Dish') {
+//                          `<h1>You should make:</h1>
+//                          <h2>${randomMain}`
+//  } else if (sideMainDessertEntree == 'Dessert'){
+//                          `<h1>You should make:</h1>
+//                          <h2>${randomDessert}`
+//  } else 
+// return 'sorry, we don\'t have that option.'
+
+}
